d4f11c6a6a3695647533fdfb25d9d533c61d97ee
kostiantyn.semchenko <kostiantyn.semchenko@masterofcode.com>
2014-06-12 18:57:08 +0300

    fix for cope

--- lib/seafile-api/file.rb
+++ lib/seafile-api/file.rb
@@ -56,7 +56,7 @@ module SeafileApi
       url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{filename}"
       c = curl_new(url,token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
-      c.http_post(Curl::PostField.content('dst_repo', dst_repo),Curl::PostField.content('dst_dir', dst_dir))
+      c.http_post(Curl::PostField.content('operation', 'move'),Curl::PostField.content('dst_repo', dst_repo),Curl::PostField.content('dst_dir', dst_dir))
     end
 
     #curl -d "dst_repo=73ddb2b8-dda8-471b-b7a7-ca742b07483c&dst_dir=/&file_names=foo.c" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' "https://cloud.seafile.com/api2/repos/c7436518-5f46-4296-97db-2fcba4c8c8db/fileops/copy/
@@ -65,7 +65,7 @@ module SeafileApi
       url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/fileops/copy/"
       c = curl_new(url,token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
-      c.http_post(Curl::PostField.content('dst_repo', dst_repo),Curl::PostField.content('dst_dir', dst_dir),Curl::PostField.content('filename', filename))
+      c.http_post(Curl::PostField.content('dst_repo', dst_repo),Curl::PostField.content('dst_dir', dst_dir),Curl::PostField.content('file_names', filename))
     end
 
     #curl -v -d "operation=rename&newname=newfoo.c" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/?p=/foo.c
