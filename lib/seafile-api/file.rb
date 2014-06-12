7f9f4f5836f37da4f241471de3b58fec16f6c843
kostiantyn.semchenko <kostiantyn.semchenko@masterofcode.com>
2014-06-12 15:23:41 +0300

    some refactor

--- lib/seafile-api/file.rb
+++ lib/seafile-api/file.rb
@@ -5,47 +5,46 @@ include URI
 module SeafileApi
   class Files
     def self::get_link(filename)
-      self::get_file_link(filename)
+      get_file_link(filename)
     end
     def self::get_token
-      self::get_sf_token
+      get_sf_token
     end
     def self::get_description(filename)
-      self::get_file_description(filename)
+      get_file_description(filename)
     end
     def self::upload(file)
-      self::put_file(file)
+      put_file(file)
     end
     def self::get_history(filename)
-      self::get_file_history(filename)
+      get_file_history(filename)
     end
     def self::update(file,target_file=nil)
-      self::update_file(file,(target_file ||File.basename(file) ))
+      update_file(file,(target_file ||File.basename(file) ))
     end
     def self::create(filename)
       create_file(filename)
     end
     private
-    def self::create_filename(filename)
-      token = self.get_sf_token
-      c = Curl::Easy.new("#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{filename}")
-      c.multipart_form_post = true
-      c.headers['Authorization'] = "Token #{token}"
+    #curl -v -d "operation=create" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/?p=/foo.c
+    def self::create_file(filename)
+      token =get_sf_token
+      url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{filename}"
+      c = curl_new(url,token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
       c.http_post(Curl::PostField.content('operation', 'create'))
-      #
-      #https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/?p=/foo.c
-      #curl -v -d "operation=create" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4'
+
     end
     #update file_
     #TODO: analise and optimization arguments needed for file upload;
     #TODO: add validation check
     #TODO: execption rescue
     def self.update_file(file,target_file)
-      token = self.get_sf_token
-      http = self.curl_get("update-link/",token)
+      token =get_sf_token
+      http = curl_get("update-link/",token)
       if is_http?(http)
-        c = curl_new(http,token)
+        url = make_url(http)
+        c = curl_new(url,token)
         c.http_post(Curl::PostField.file('file', file),Curl::PostField.content('filename', File.basename(file)),Curl::PostField.content('target_file', "/#{(target_file)}"))
       else
         "something wrong #{http.body_str}"
@@ -67,10 +66,11 @@ module SeafileApi
 =end
 
     def self.put_file(file)
-      token = self.get_sf_token
-      http = self.curl_get("upload-link/",token)
+      token = get_sf_token
+      http = curl_get("upload-link/",token)
       if is_http?(http)
-         c = curl_new(http,token)
+         url = make_url(http)
+         c = curl_new(url,token)
          c.http_post(Curl::PostField.file('file', file),Curl::PostField.content('filename',File.basename(file)) ,Curl::PostField.content('parent_dir', '/'))
       else
         "something wrong #{http.body_str}"
@@ -95,24 +95,24 @@ module SeafileApi
     # 404 File not found
     # 520 Operation failed.
     def self.get_file_link(filename)
-      token = self.get_sf_token
-      http = self.curl_get("file/?p=/#{filename}",token)
+      token =get_sf_token
+      http = curl_get("file/?p=/#{filename}",token)
       result(http.body_str.gsub('"', ''))
     end
 
     #curl -H 'Authorization: Token f2210dacd3606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/detail/?p=/foo.c
     #get file description
     def self.get_file_description(filename)
-      token = self.get_sf_token
-      http = self.curl_get("file/detail/?p=/#{filename}",token)
+      token = get_sf_token
+      http = curl_get("file/detail/?p=/#{filename}",token)
       result(http.body_str.gsub('"', ''))
     end
 
     #curl -H 'Authorization: Token f2210dacd3606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/history/?p=/foo.c
     #get file History
     def self.get_file_history(filename)
-      token = self.get_sf_token
-      http = self.curl_get("file/history/?p=/#{filename}",token)
+      token = get_sf_token
+      http = curl_get("file/history/?p=/#{filename}",token)
       self.result(http.body_str.gsub('"', ''))
     end
 
@@ -131,10 +131,13 @@ module SeafileApi
       end
     end
     def self.is_http?(http)
-      http.body_str.gsub('"', '').scan('http').empty?
+      !http.body_str.gsub('"', '').scan('http').empty?
     end
-    def self.curl_new(http,token)
-      url = http.body_str.gsub('"', '')
+    def self.make_url(http)
+      http.body_str.gsub('"', '')
+    end
+    def self.curl_new(url,token)
+
       c = Curl::Easy.new(url)
       c.multipart_form_post = true
       c.headers['Authorization'] = "Token #{token}"
