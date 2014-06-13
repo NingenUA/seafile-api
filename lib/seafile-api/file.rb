e77c2ca2c1f89ae6c0b474ed372b8f7b44ac72cf
kostiantyn.semchenko <kostiantyn.semchenko@masterofcode.com>
2014-06-13 14:55:10 +0300

    add all other function for file working

--- lib/seafile-api/file.rb
+++ lib/seafile-api/file.rb
@@ -19,6 +19,9 @@ module SeafileApi
     def self::get_history(filename)
       get_file_history(filename)
     end
+    def self::get_block_link
+      get_block
+    end
 
     def self::upload(file)
       put_file(file)
@@ -39,42 +42,81 @@ module SeafileApi
     def self::move(filename,dst_dir='/',dest_repo=SeafileApi::Config.repo)
       move_file(filename,dst_dir,dest_repo)
     end
+
+    def self::delete(filename)
+      delete_file(filename)
+    end
+=begin  not working
+    def self::revert(filename,commit_id)
+      revert_file(filename,commit_id)
+    end
+=end
     private
     #!!!!!!!!!operation with file sections
+    #curl -X DELETE -v  -H 'Authorization: Token f2210dacd3606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32
+    def self.delete_file(filename)
+      token =get_sf_token
+      url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{filename}"
+      c = curl_new(URI::encode(url),token)
+      c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
+      c.http_delete()
+      c.body_str
+    end
+
+    #TODO: Revert  is not working, some problem with put and 2  send objects
+    #curl -v -X PUT -d "commit_id=a1ec20709675f4dc8db825cdbca296be245d189b&p=/foo.c" -H "Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd" -H 'Accept: application/json; indent=4' https://cloud.seafile.com/api2/repos/8f5f2222-72a8-454f-ac40-8397c5a556a8/file/revert/
+    def self.revert_file(filename,commit_id)
+      token =get_sf_token
+      url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/revert/"
+      http = Curl.put(URI::encode(url)) do |http|
+        http.multipart_form_post = true
+        http.headers['Authorization'] = "Token #{token}"
+        http.headers['Accept'] = "application/json; charset=utf-8; indent=4"
+        http.content['commit_id']=commit_id
+        http.content['p']= filename
+      end
+      http.body_str
+    end
+
+
     #curl -v -d "operation=create" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/?p=/foo.c
     def self.create_file(filename)
       token =get_sf_token
       url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{filename}"
-      c = curl_new(url,token)
+      c = curl_new(URI::encode(url),token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
       c.http_post(Curl::PostField.content('operation', 'create'))
+      c.body_str
     end
 
     #curl -v -d "operation=move&dst_repo=affc837f-7fdd-4e91-b88a-32caf99897f2&dst_dir=/" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/?p=/foo.c
     def self.move_file(filename,dst_dir,dst_repo)
       token =get_sf_token
       url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{filename}"
-      c = curl_new(url,token)
+      c = curl_new(URI::encode(url),token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
       c.http_post(Curl::PostField.content('operation', 'move'),Curl::PostField.content('dst_repo', dst_repo),Curl::PostField.content('dst_dir', dst_dir))
+      c.body_str
     end
 
     #curl -d "dst_repo=73ddb2b8-dda8-471b-b7a7-ca742b07483c&dst_dir=/&file_names=foo.c" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' "https://cloud.seafile.com/api2/repos/c7436518-5f46-4296-97db-2fcba4c8c8db/fileops/copy/
     def self.copy_file(filename,dst_dir,dst_repo)
       token =get_sf_token
       url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/fileops/copy/"
-      c = curl_new(url,token)
+      c = curl_new(URI::encode(url),token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
       c.http_post(Curl::PostField.content('dst_repo', dst_repo),Curl::PostField.content('dst_dir', dst_dir),Curl::PostField.content('file_names', filename))
+      c.body_str
     end
 
     #curl -v -d "operation=rename&newname=newfoo.c" -H 'Authorization: Token f2210dacd9c6ccb8133606d94ff8e61d99b477fd' -H 'Accept: application/json; charset=utf-8; indent=4' https://cloud.seafile.com/api2/repos/dae8cecc-2359-4d33-aa42-01b7846c4b32/file/?p=/foo.c
     def self.rename_file(old_name,new_name)
       token =get_sf_token
       url = "#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/file/?p=/#{old_name}"
-      c = curl_new(url,token)
+      c = curl_new(URI::encode(url),token)
       c.headers['Accept'] = "application/json; charset=utf-8; indent=4"
       c.http_post(Curl::PostField.content('operation', 'rename'),Curl::PostField.content('newname', new_name))
+      c.body_str
     end
 
 
@@ -88,8 +130,9 @@ module SeafileApi
       http = curl_get("update-link/",token)
       if is_http?(http)
         url = cl_body_str(http)
-        c = curl_new(url,token)
+        c = curl_new(URI::encode(url),token)
         c.http_post(Curl::PostField.file('file', file),Curl::PostField.content('filename', File.basename(file)),Curl::PostField.content('target_file', "/#{(target_file)}"))
+        c.body_str
       else
         "something wrong #{http.body_str}"
       end
@@ -114,8 +157,9 @@ module SeafileApi
       http = curl_get("upload-link/",token)
       if is_http?(http)
          url = cl_body_str(http)
-         c = curl_new(url,token)
+         c = curl_new(URI::encode(url),token)
          c.http_post(Curl::PostField.file('file', file),Curl::PostField.content('filename',File.basename(file)) ,Curl::PostField.content('parent_dir', '/'))
+         c.body_str
       else
         "something wrong #{http.body_str}"
       end
@@ -150,6 +194,11 @@ module SeafileApi
     def self.get_file_link(filename)
       token =get_sf_token
       http = curl_get("file/?p=/#{filename}",token)
+      result(cl_body_str(http))
+    end
+    def self.get_block
+      token =get_sf_token
+      http = curl_get("upload-blks-link/",token)
       result(cl_body_str(http))
     end
 
@@ -193,14 +242,14 @@ module SeafileApi
       http.body_str.gsub('"', '')
     end
     def self.curl_new(url,token)
-
       c = Curl::Easy.new(url)
       c.multipart_form_post = true
       c.headers['Authorization'] = "Token #{token}"
       c
     end
+
     def self.curl_get(url,token)
-      Curl.get("#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/#{url}") do|http|
+      Curl.get(URI::encode("#{SeafileApi::Config.host}/api2/repos/#{SeafileApi::Config.repo}/#{url}")) do|http|
         http.headers['Authorization'] = "Token #{token}"
       end
     end
